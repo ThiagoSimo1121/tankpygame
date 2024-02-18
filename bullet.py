7bdc2f53c5d61737494d61a9b6e655bb2100e1ed
Benicio Mozan Santos Vale <149823225+BenicioMozan1@users.noreply.github.com>
2024-02-17 22:42:26 -0400

    Adding bullet collision with walls, and a "damage" function.

--- bullet.py
+++ bullet.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 
+
 class Bullet:
     def __init__(self, dono ,x, y, angulo, velocidade, direção_bola):
         self.dono = dono
@@ -8,7 +9,8 @@ class Bullet:
         self.angulo = angulo
         self.velocidade = velocidade
         self.size = 5
-        self.color = (255, 255, 255)  # Cor branca
+        self.color = (0,0,0)  # Cor branca
+        self.num_of_collision = 0
         # Ajusta a posição inicial da bala
         comprimento_tank = 45  # Substitua pelo comprimento real do seu tanque
         self.x = x + comprimento_tank * math.cos(math.radians(self.angulo)) * self.direção_bola
@@ -22,10 +24,38 @@ class Bullet:
 
     def draw(self, tela):
         pygame.draw.rect(tela, self.color, self.rect)
+        #hitbox da bala
+        pygame.draw.rect(tela, (255,255,255), self.rect, 1)
 
-    def collision(self):
+    def collision_screen(self):
         if self.x >= 1440 or self.x <= 0:
-            self.angulo = 180 - self.angulo  # Reflete o ângulo na direção x
+            self.angulo = 180 - self.angulo 
+            # Reflete o ângulo na direção x
         if self.y >= 750 or self.y <= 0:
-            self.angulo = -self.angulo  # Reflete o ângulo na direção y
+            self.angulo = -self.angulo  
+            # Reflete o ângulo na direção y
+
+    def collision_walls(self, world):
+        for tile in world.tile_list:
+            if tile[1].colliderect(self.rect):
+                # Verifica colisão na direção x
+                if self.x + 5>= tile[1].right or self.x <= tile[1].left:
+                    self.angulo = 180 - self.angulo
+                    self.num_of_collision += 1
+                # Verifica colisão na direção y
+                elif self.y >= tile[1].top or self.y <= tile[1].bottom:
+                    self.angulo = -self.angulo
+                    self.num_of_collision += 1
+    
+    def get_num_of_collisions(self):
+        return self.num_of_collision
+    
+    def hit_tank(self, tank):
+        tank.vida -= 1
+
+
+
+
+        
+
 
